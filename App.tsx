fad57f2d3086a0164b217616adc5c0212c3e55d6
Tierry Barros <tierry@MacBook-Air-de-Tierry.local>
2023-11-02 22:58:16 -0300

    Authentication ios and android

--- App.tsx
+++ App.tsx
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+
+import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  async function verifyAvaiableAuthentication() {
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+    console.log(compatible);
+
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    console.log(types);
+  }
+
+  async function handleAuthentication() {
+    const isBiometricEnrolled = await LocalAuthentication.isEnrolledAsync();
+
+    if (!isBiometricEnrolled) {
+      return Alert.alert(
+        'Login',
+        'Nenhuma biometria cadastrada. Por favor cadastre.'
+      );
+    }
+
+    const auth = await LocalAuthentication.authenticateAsync({
+      promptMessage: 'Login com biometria',
+      fallbackLabel: 'Biometria não reconhecida',
+    });
+
+    setIsAuthenticated(auth.success);
+  }
+
+  useEffect(() => {
+    verifyAvaiableAuthentication();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Usuario conectado: {isAuthenticated ? 'Sim' : 'Não'}</Text>
+      <Button title="Entrar" onPress={handleAuthentication} />
     </View>
   );
 }
